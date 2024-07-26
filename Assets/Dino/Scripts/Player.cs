115ece30b0f77622b7f4498c48954e6f770b3e40
Mando <codefilez@gmail.com>
2024-07-26 15:08:16 -0700

    Dino physics movements implemented, with out of boundary level colliders.

--- Assets/Dino/Scripts/Player.cs
+++ Assets/Dino/Scripts/Player.cs
@@ -18,17 +18,22 @@ namespace Assets.Dino.Scripts
             Jump = 4
         };
 
+        const float RUN_SPEED = 15f;
+        const float NO_SPEED = 0f;
         const string ANIMATOR_STATE_PARAMETER_LABEL = "AnimationState";
 
         public GameObject dino { get; set; }
         public SpriteRenderer spriteRenderer { get; set; }
         public Animator animator { get; set; }
+        public Rigidbody2D rigidBody { get; set; }
+
         public Player(GameObject dinoGameObject) 
         {
             // Get GameObject Components.
             dino = dinoGameObject;
             spriteRenderer = dino.GetComponent<SpriteRenderer>();
             animator = dino.GetComponent<Animator>();
+            rigidBody = dino.GetComponent<Rigidbody2D>();
 
             // Set Initial Animation State.
             UpdateAnimationState(ANIMATION_STATES.Idle);
@@ -37,7 +42,29 @@ namespace Assets.Dino.Scripts
         public void UpdateAnimationState(ANIMATION_STATES animationState)
         {
             animator.SetInteger(ANIMATOR_STATE_PARAMETER_LABEL, (int)animationState);
+            Move(animationState);
         }
+
+        public void Move(ANIMATION_STATES animationState)
+        {
+            switch(animationState)
+            {
+                case ANIMATION_STATES.FastRun: FastRun(); 
+                    break;
+                default: DescendRun();
+                    break;
+            }
+        }
+
+        protected void FastRun()
+        {
+            rigidBody.AddForce(new Vector2(RUN_SPEED, NO_SPEED));
+        }
+
+        protected void DescendRun()
+        {
+            rigidBody.AddForce(new Vector2(-(RUN_SPEED / 2), NO_SPEED));
+        } 
 
     }
 }
